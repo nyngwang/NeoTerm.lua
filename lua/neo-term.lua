fc68251f17ce79ead4a44f88f1ef4d068a3de233
nyngwang <nyngwang@gmail.com>
2022-12-07 23:35:33 +0800

    fix: NeoTerm is now cursor friendly :rocket:

--- lua/neo-term.lua
+++ lua/neo-term.lua
@@ -5,31 +5,24 @@ vim.api.nvim_create_augroup('neo-term.lua', { clear = true })
 -------------------------------------------------------------------------------------------------------
 local M = { }
 local _parent_buf_to_term_buf = { }
-local _split_wins = { }
-local _restore_cursor = false
+local _enter_views = { }
+local _open_win_to_term_win = { }
 
-local function found_buf_in_tabpage(t, b)
-  if b == nil then return -1 end
-  for _, w in ipairs(vim.api.nvim_tabpage_list_wins(t)) do
-    if vim.api.nvim_win_get_buf(w) == b then
-      return w
-    end
-  end
-  return -1
-end
 
 local function remove_invalid_mappings()
-  local to_remove = {}
   for i, v in ipairs(_parent_buf_to_term_buf) do
     if not (
         vim.api.nvim_buf_is_valid(i)
         and vim.api.nvim_buf_is_valid(v)
       ) then
-      to_remove[#to_remove+1] = i
+      _parent_buf_to_term_buf[i] = nil
     end
   end
-  for _, v in ipairs(to_remove) do
-    table.remove(_parent_buf_to_term_buf, v)
+  for i, v in ipairs(_open_win_to_term_win) do
+    if v ~= nil
+      and not vim.api.nvim_win_is_valid(v) then
+      _open_win_to_term_win[i] = nil
+    end
   end
 end
 
@@ -91,27 +84,33 @@ function M.open_termbuf()
   for _, v in ipairs(M.exclude_filetypes) do if vim.bo.filetype == v then return end end
   for _, v in ipairs(M.exclude_buftypes) do if vim.bo.buftype == v then return end end
 
+  local open_win = vim.api.nvim_get_current_win()
+  local _term_win = _open_win_to_term_win[open_win]
+  -- should leave immediately on open_win hit.
+  if _term_win ~= nil and vim.api.nvim_win_is_valid(_term_win) then
+    vim.api.nvim_set_current_win(_term_win)
+    return
+  end
+
   local parent_buf = vim.api.nvim_get_current_buf()
   local parent_win_height = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].height
   local termbuf_size = parent_win_height * M.split_size
   local parent_size = parent_win_height - termbuf_size
 
-  if vim.api.nvim_buf_get_name(parent_buf) == ''
-  then _restore_cursor = false
-  else _restore_cursor = true end
-
-  local win_of_termbuf = found_buf_in_tabpage(0, _parent_buf_to_term_buf[parent_buf])
-  if win_of_termbuf ~= -1 and _split_wins[win_of_termbuf] then
-    local cur_win = vim.api.nvim_get_current_win()
-    table.remove(_split_wins, win_of_termbuf)
-    vim.api.nvim_set_current_win(win_of_termbuf)
-    vim.cmd('q')
-    vim.api.nvim_set_current_win(cur_win)
-    return
-  end
+  -- TRY: don't need to close existing termbuf.
+  -- local win_of_termbuf = found_buf_in_tabpage(0, _parent_buf_to_term_buf[parent_buf])
+  -- if win_of_termbuf ~= -1 and _split_wins[win_of_termbuf] then
+  --   local cur_win = vim.api.nvim_get_current_win()
+  --   table.remove(_split_wins, win_of_termbuf)
+  --   vim.api.nvim_set_current_win(win_of_termbuf)
+  --   vim.cmd('q')
+  --   vim.api.nvim_set_current_win(cur_win)
+  --   return
+  -- end
 
   -- this makes things easier
   local _splitbelow = vim.opt.splitbelow
+  _enter_views[vim.api.nvim_get_current_buf()] = vim.fn.winsaveview()
   if M.split_on_top then
     vim.opt.splitbelow = false
   else
@@ -128,9 +127,10 @@ function M.open_termbuf()
   vim.cmd('wincmd p')
   vim.cmd('resize ' .. parent_size)
   vim.cmd('wincmd p') -- cursor at termbuf split
-  _split_wins[vim.api.nvim_get_current_win()] = true
+  local term_win = vim.api.nvim_get_current_win()
+  _open_win_to_term_win[open_win] = term_win
 
-  if -- termbuf for this win exists
+  if -- termbuf for this buf exists
     _parent_buf_to_term_buf[parent_buf] ~= nil
     and vim.api.nvim_buf_is_valid(_parent_buf_to_term_buf[parent_buf]) then
     vim.api.nvim_set_current_buf(_parent_buf_to_term_buf[parent_buf])
@@ -143,11 +143,19 @@ function M.open_termbuf()
 end
 
 function M.close_termbuf()
-  vim.cmd('NeoTermEnterNormal') -- exit term-insert-mode or do nothing
-  if _split_wins[vim.api.nvim_get_current_win()] then
-    table.remove(_split_wins, vim.api.nvim_get_current_win())
-    vim.cmd('q')
-    if _restore_cursor then vim.cmd('normal! ') end
+  -- should only work on terminal buffer.
+  if not vim.bo.buftype == 'terminal' then return end
+
+  -- we also do this again in case the user forgot to call it.
+  vim.cmd('NeoTermEnterNormal')
+
+  -- close it anyway, and restore view as long as it's parent_buf.
+  local term_buf = vim.api.nvim_get_current_buf()
+  vim.cmd('q')
+  local final_buf = vim.api.nvim_get_current_buf()
+  if _parent_buf_to_term_buf[final_buf] == term_buf then
+    -- restore back to the enter view.
+    vim.fn.winrestview(_enter_views[final_buf])
   end
 end
 
