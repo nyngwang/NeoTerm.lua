fa03d1421964351fcf466597092b00f99a775ab5
nyngwang <nyngwang@gmail.com>
2023-02-27 10:46:01 +0800

    refactor(autocmds): make use of `filetype` of value `neo-term`

--- lua/neo-term/utils/autocmd.lua
+++ lua/neo-term/utils/autocmd.lua
@@ -2,26 +2,26 @@ local M = {}
 
 
 function M.create_autocmds()
-  -- change bg-color on enter term-mode.
+  -- change bg-color on enter term-mode of NeoTerm.
   vim.api.nvim_create_autocmd({ 'TermEnter' }, {
     group = 'neo-term.lua',
     pattern = '*',
     callback = function ()
-      if not vim.api.nvim_buf_get_option(0, 'buflisted') then return end
+      if vim.bo.filetype ~= 'neo-term' then return end
       vim.cmd('hi NEO_TERM_COOL_BLACK guibg=#101010')
       vim.cmd('set winhl=Normal:' .. require('neo-term').term_mode_hl)
     end
   })
-  -- reset bg-color on leave term-mode.
+  -- reset bg-color on leave term-mode of NeoTerm.
   vim.api.nvim_create_autocmd({ 'TermLeave' }, {
     group = 'neo-term.lua',
     pattern = '*',
     callback = function ()
-      if not vim.api.nvim_buf_get_option(0, 'buflisted') then return end
+      if vim.bo.filetype ~= 'neo-term' then return end
       vim.cmd('set winhl=')
     end
   })
-  -- auto-insert on enter term-buf.
+  -- auto-insert on enter term-buf of NeoTerm.
   vim.api.nvim_create_autocmd({ 'BufEnter' }, {
     group = 'neo-term.lua',
     pattern = '*',
