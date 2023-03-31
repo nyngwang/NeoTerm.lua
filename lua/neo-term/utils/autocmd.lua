5e33b29a2299f32e279c615e416189acdb6244c2
nyngwang <nyngwang@gmail.com>
2023-03-31 19:27:35 +0800

    fix(autocmd): `:q` is layout-preserving for non-floatin term-buf now (Close #23)

--- lua/neo-term/utils/autocmd.lua
+++ lua/neo-term/utils/autocmd.lua
@@ -1,7 +1,39 @@
 local M = {}
 
 
-function M.create_autocmds()
+local function create_hotfix_autocmds()
+  -- for the upstream issue: https://github.com/neovim/neovim/issues/5176.
+  -- NOTE: this doesn't include the case of rpc, which is `WinClosed` on a normal buffer.
+  vim.api.nvim_create_autocmd({ 'WinClosed' }, {
+    group = 'neo-term.lua',
+    callback = function (args)
+      if vim.bo.filetype ~= 'neo-term'
+        or vim.bo.buftype ~= 'terminal'
+      then return end
+
+      if -- it's another buffer/win being closed.
+        vim.api.nvim_get_current_buf() ~= args.buf
+        or -- floating window
+          vim.api.nvim_win_get_config(0).relative ~= ''
+      then return end
+
+      for o, t in pairs(require('neo-term').buf_open_to_term) do
+        if vim.api.nvim_get_current_buf() == t
+        then
+          -- NOTE: the order matters.
+          -- should restore view AFTER split.
+          vim.cmd('vsplit')
+          vim.api.nvim_set_current_buf(o)
+          vim.fn.winrestview(require('neo-term').view_of_open_buf[o])
+          return
+        end
+      end
+    end,
+  })
+end
+
+
+local function create_features_autocmds()
   -- change bg-color on enter term-mode of NeoTerm.
   vim.api.nvim_create_autocmd({ 'TermEnter' }, {
     group = 'neo-term.lua',
@@ -47,6 +79,12 @@ function M.create_autocmds()
       vim.cmd('stopinsert')
     end
   })
+end
+
+
+function M.create_autocmds()
+  create_features_autocmds()
+  create_hotfix_autocmds()
 end
 
 
